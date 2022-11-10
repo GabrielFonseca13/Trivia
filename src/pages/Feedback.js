b5a52bcc53504de04a19e02dfbd0311a30ff616b
Rafael Musse <rafaelmusse@MacBook-Air-de-Rafael.local>
2022-11-10 15:20:21 -0300

    adicionando PropTypes e connect

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { score, assertions } = this.props;
     const averageAnswers = 3;
     return (
       <div>
         <Header />
-        <p data-testid="feedback-total-score">Valor final provisorio</p>
-        <p data-testid="feedback-total-question"> Quantidade de respostas acertadas</p>
-        {rightAnswers >= averageAnswers ? <p data-testid="feedback-text">Well Done!</p>
+        <p data-testid="feedback-total-score">{score}</p>
+        <p data-testid="feedback-total-question">{assertions}</p>
+        {assertions >= averageAnswers ? <p data-testid="feedback-text">Well Done!</p>
           : <p ata-testid="feedback-text">Could be better...</p>}
       </div>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  score: state.player.score,
+  assertions: state.player.assertions,
+});
 
-export default connect()(Feedback);
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
